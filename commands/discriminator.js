364b66685cdeda1ecfd060bc5dd11ee91b332f72
Jacob Gunther <jacobgunther12@gmail.com>
2017-08-28 15:59:35 -0500

    Finished first part of discrim command

---
+++ commands/discriminator.js
@@ -0,0 +1,62 @@
+const snekfetch = require("snekfetch");
+const config = require("../config.json");
+
+module.exports = {
+    commands: [
+        "discriminator",
+        "discrim"
+    ],
+    description: "Find users with the same discriminator as you.",
+    usage: "discriminator <discrim> [page #]",
+    category: "Information",
+    hidden: false,
+    execute: (bot, database, msg, args) => {
+        if (args.length < 1) args[0] = msg.author.discriminator;
+        if (isNaN(Number(args[0]))) return msg.channel.send({
+            embed: {
+                title: "Error!",
+                color: 0xE50000,
+                description: "`" + args[0] + "` is not a valid number."
+            }
+        });
+        if (Number(args[0]) > 9999) return msg.channel.send({
+            embed: {
+                title: "Error!",
+                color: 0xE50000,
+                description: "Discriminators are four digit numbers."
+            }
+        });
+        if (Number(args[0]) < 1) return msg.channel.send({
+            embed: {
+                title: "Error!",
+                color: 0xE50000,
+                description: "Discriminators do not go below 1."
+            }
+        });
+        const users = bot.users.filter(u => u.discriminator === args[0] && u.id !== msg.author.id).map(u => u.tag);
+        if (users.length > 0) {
+            if (args.length > 1) {
+
+            } else {
+                msg.channel.send({
+                    embed: {
+                        title: "Discriminators",
+                        color: 3066993,
+                        description: users.slice(0, 25).join("\n"),
+                        footer: {
+                            text: "Page 1 / " + Math.ceil(users.length / 25)
+                        }
+                    }
+                });
+            }
+        } else {
+            msg.channel.send({
+                embed: {
+                    title: "Error!",
+                    color: 0xE500000,
+                    description: "There are no users found with that discriminator."
+                }
+            });
+        }
+    }
+};
