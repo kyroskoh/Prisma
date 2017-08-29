816f74721453a2c080d9fa9fb7e1564b66058bd4
Jacob Gunther <jacobgunther12@gmail.com>
2017-08-28 21:15:48 -0500

    Blur now can accept users, fixed up ban, not complete

--- commands/ban.js
+++ commands/ban.js
@@ -1,4 +1,4 @@
-const snekfetch = require("snekfetch");
+const resolveUser = require("../functions/resolve-user.js");
 
 module.exports = {
     commands: [
@@ -10,6 +10,33 @@ module.exports = {
     category: "Moderation",
     hidden: false,
     execute: (bot, database, msg, args) => {
-        
+        if (msg.channel.type === "dm") return msg.channel.send({
+            embed: {
+                title: "Error!",
+                color: 0xE50000,
+                description: "This command cannot be used in a Direct Message."
+            }
+        });
+        if (msg.member.hasPermission("BAN_MEMBERS")) {
+            if (msg.guild.me.hasPermission("BAN_MEMBERS")) {
+                // todo
+            } else {
+                msg.channel.send({
+                    embed: {
+                        title: "Error!",
+                        color: 0xE50000,
+                        description: "I do not have permission to ban members. Please give me the `Ban Members` command."
+                    }
+                });
+            }
+        } else {
+            msg.channel.send({
+                embed: {
+                    title: "Error!",
+                    color: 0xE50000,
+                    description: "You do not have permission to use this command. This command requires the `Ban Members` permission."
+                }
+            });
+        }
     }
 };
--- commands/blur.js
+++ commands/blur.js
@@ -1,79 +1,90 @@
 const snekfetch = require("snekfetch");
 const gm = require("gm");
+const resolveUser = require("../functions/resolve-user.js");
 
 module.exports = {
     commands: [
         "blur"
     ],
     description: "Blurs an image a certain percent.",
-    usage: "blur <url> [amount]",
+    usage: "blur <url | @user | username | userID> [amount]",
     category: "Image",
     hidden: false,
     execute: (bot, database, msg, args) => {
         if (args.length > 0) {
-            if (!/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/.test(args[0])) return msg.channel.send({
-                embed: {
-                    title: "Error!",
-                    color: 0xE50000,
-                    description: "Invalid image URL."
-                }
-            });
-            let amount = 15;
-            if (args.length > 1) {
-                if (isNaN(Number(args[1].replace("%", "")))) return msg.channel.send({
-                    embed: {
-                        title: "Error!",
-                        color: 0xE50000,
-                        description: "`" + args[1] + "` is not a valid number."
-                    }
-                });
-                if (Number(args[1].replace("%", "")) > 100) return msg.channel.send({
-                    embed: {
-                        title: "Error!",
-                        color: 0xE50000,
-                        description: "The blur amount cannot be greater than 100%."
-                    }
-                });
-                if (Number(args[1].replace("%", "")) < 1) return msg.channel.send({
-                    embed: {
-                        title: "Error!",
-                        color: 0xE50000,
-                        description: "The blur amount cannot be less than 1%."
-                    }
-                });
-                amount = Number(args[1].replace("%", ""));
-            }
-            snekfetch.get(args[0]).then(body => {
-                try {
-                    gm(body.body).blur(50, amount).toBuffer((error, buffer) => {
-                        if (error) return console.error(error);
-                        msg.channel.send({
-                            files: [
-                                {
-                                    attachment: buffer,
-                                    name: "image.png"
-                                }
-                            ]
-                        });
-                    });
-                } catch(e) {
+            if (/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/.test(args[0])) {
+                next(args[0]);
+            } else {
+                resolveUser(bot, args[0]).then(user => {
+                    next(user.avatarURL);
+                }).catch(error => {
                     msg.channel.send({
                         embed: {
                             title: "Error!",
                             color: 0xE50000,
-                            description: "An error occured while editing image."
+                            description: "Unable to find any users by that search."
                         }
                     });
-                }
-            }).catch(error => {
-                msg.channel.send({
-                    embed: {
-                        title: "Error!",
-                        color: 0xE50000,
-                        description: "Failed to load image. `" + error.message + "`"
-                    }
                 });
-            });
+            }
+            function next(url) {
+                let amount = 15;
+                if (args.length > 1) {
+                    if (isNaN(Number(args[1].replace("%", "")))) return msg.channel.send({
+                        embed: {
+                            title: "Error!",
+                            color: 0xE50000,
+                            description: "`" + args[1] + "` is not a valid number."
+                        }
+                    });
+                    if (Number(args[1].replace("%", "")) > 100) return msg.channel.send({
+                        embed: {
+                            title: "Error!",
+                            color: 0xE50000,
+                            description: "The blur amount cannot be greater than 100%."
+                        }
+                    });
+                    if (Number(args[1].replace("%", "")) < 1) return msg.channel.send({
+                        embed: {
+                            title: "Error!",
+                            color: 0xE50000,
+                            description: "The blur amount cannot be less than 1%."
+                        }
+                    });
+                    amount = Number(args[1].replace("%", ""));
+                }
+                snekfetch.get(url).then(body => {
+                    try {
+                        gm(body.body).blur(25, amount).toBuffer((error, buffer) => {
+                            if (error) return console.error(error);
+                            msg.channel.send({
+                                files: [
+                                    {
+                                        attachment: buffer,
+                                        name: "image.png"
+                                    }
+                                ]
+                            });
+                        });
+                    } catch (e) {
+                        msg.channel.send({
+                            embed: {
+                                title: "Error!",
+                                color: 0xE50000,
+                                description: "An error occured while editing image."
+                            }
+                        });
+                    }
+                }).catch(error => {
+                    msg.channel.send({
+                        embed: {
+                            title: "Error!",
+                            color: 0xE50000,
+                            description: "Failed to load image. `" + error.message + "`"
+                        }
+                    });
+                });
+            }
         } else {
             msg.channel.send({
                 embed: {
