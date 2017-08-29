642d159baf82f5244d5d3d7bc080500b891e052f
Jacob Gunther <jacobgunther12@gmail.com>
2017-08-28 21:28:20 -0500

    Added quality command

---
+++ commands/quality.js
@@ -0,0 +1,92 @@
+const snekfetch = require("snekfetch");
+const gm = require("gm");
+const resolveUser = require("../functions/resolve-user.js");
+
+module.exports = {
+    commands: [
+        "quality"
+    ],
+    description: "Changed the quality of an image.",
+    usage: "blur [url | @user | username | userID] [amount]",
+    category: "Image",
+    hidden: false,
+    execute: (bot, database, msg, args) => {
+        if (args.length > 0) {
+            if (/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/.test(args[0])) {
+                next(args[0]);
+            } else {
+                resolveUser(bot, args[0]).then(user => {
+                    next(user.avatarURL);
+                }).catch(error => {
+                    msg.channel.send({
+                        embed: {
+                            title: "Error!",
+                            color: 0xE50000,
+                            description: "Unable to find any users by that search."
+                        }
+                    });
+                });
+            }
+        } else {
+            next(msg.author.avatarURL);
+        }
+        function next(url) {
+            let amount = 50;
+            if (args.length > 1) {
+                if (isNaN(Number(args[1].replace("%", "")))) return msg.channel.send({
+                    embed: {
+                        title: "Error!",
+                        color: 0xE50000,
+                        description: "`" + args[1] + "` is not a valid number."
+                    }
+                });
+                if (Number(args[1].replace("%", "")) > 100) return msg.channel.send({
+                    embed: {
+                        title: "Error!",
+                        color: 0xE50000,
+                        description: "The blur amount cannot be greater than 100%."
+                    }
+                });
+                if (Number(args[1].replace("%", "")) < 1) return msg.channel.send({
+                    embed: {
+                        title: "Error!",
+                        color: 0xE50000,
+                        description: "The quality amount cannot be less than 1%."
+                    }
+                });
+                amount = Number(args[1].replace("%", ""));
+            }
+            snekfetch.get(url).then(body => {
+                try {
+                    gm(body.body).quality(amount).toBuffer((error, buffer) => {
+                        if (error) return console.error(error);
+                        msg.channel.send({
+                            files: [
+                                {
+                                    attachment: buffer,
+                                    name: "image.png"
+                                }
+                            ]
+                        });
+                    });
+                } catch (e) {
+                    msg.channel.send({
+                        embed: {
+                            title: "Error!",
+                            color: 0xE50000,
+                            description: "An error occured while editing image."
+                        }
+                    });
+                }
+            }).catch(error => {
+                msg.channel.send({
+                    embed: {
+                        title: "Error!",
+                        color: 0xE50000,
+                        description: "Failed to load image. `" + error.message + "`"
+                    }
+                });
+            });
+        }
+    }
+};
