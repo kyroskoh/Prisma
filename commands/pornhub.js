5552d9784ebf83148718421d278d1f7f5d869a8b
Jacob Gunther <jacobgunther12@gmail.com>
2017-08-29 20:04:10 -0500

    Added pornhub command, just testing

---
+++ commands/pornhub.js
@@ -0,0 +1,48 @@
+const pornhub = require("pornhub");
+
+module.exports = {
+    commands: [
+        "pornhub",
+        "ph"
+    ],
+    usage: "pornhub <search query>",
+    description: "Finds a video on PornHub.",
+    category: "NSFW",
+    hidden: false,
+    execute: (bot, database, msg, args) => {
+        if (!msg.channel.nsfw) return msg.channel.send({
+            embed: {
+                title: "Error!",
+                color: 0xE50000,
+                description: "This command can only be used in a NSFW channel."
+            }
+        });
+        if (args.length > 0) {
+            pornhub.search({
+                search: args.join(" "),
+                thumbnail: "large_hd"
+            }, (error, data) => {
+                if (error) {
+                    msg.channel.send({
+                        embed: {
+                            title: "Error!",
+                            color: 0xE50000,
+                            description: "An unexpected error occured while searching for that video."
+                        }
+                    });
+                    console.error(error);
+                } else {
+                    console.log(data);
+                }
+            });
+        } else {
+            msg.channel.send({
+                embed: {
+                    title: "Error!",
+                    color: 0xE50000,
+                    description: "Missing `<search query>` option."
+                }
+            });
+        }
+    }
+};
