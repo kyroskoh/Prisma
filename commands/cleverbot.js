ce9b4deb0612c45786bf45ccd8e1dd802f3abd40
Jacob Gunther <jacobgunther12@gmail.com>
2017-08-30 16:31:34 -0500

    Removed logs scheduler

--- commands/cleverbot.js
+++ commands/cleverbot.js
@@ -18,13 +18,6 @@ module.exports = {
     hidden: false,
     execute: (bot, database, msg, args) => {
         if (args.length > 0) {
-            /* return msg.channel.send({
-                embed: {
-                    title: "Error!",
-                    color: 0xE50000,
-                    description: "Cleverbot is now charging for their services, so we are unable to have a cleverbot. If you want to make this command work again, please donate using `" + ((msg.guild) ? msg.guild.data.prefix : config.prefix) + "donate`."
-                }
-            }); */
             clever.ask(args.join(" "), (error, response) => {
                 if (error) {
                     msg.channel.send({
