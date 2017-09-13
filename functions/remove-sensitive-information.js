1b29cfc7293afe32615caaf03caddbcbf9e63bf0
Jacob Gunther <jacobgunther12@gmail.com>
2017-09-12 22:05:37 -0500

    Removed more sensitive information from eval

--- functions/remove-sensitive-information.js
+++ functions/remove-sensitive-information.js
@@ -1,5 +1,19 @@
 const config = require("../config.json");
 
 module.exports = (data) => {
-	return data.split(config.token).join("-- BOT TOKEN --").split(config.api_keys.bot_list["discordbots.org"]).join("-- discordbots.org API KEY --").split(config.api_keys.bot_list["bots.discord.pw"]).join("-- bots.discord.pw API KEY").split(config.api_keys.datadog.api_key).join("-- DATADOG API KEY --").split(config.api_keys.datadog.app_key).join("-- DATADOG APP KEY --").split(config.api_keys.youtube).join("-- YOUTUBE API KEY --").split(config.api_keys.steam).join("-- STEAM API KEY --").split(config.api_keys.thecatapi).join("-- RANDOM CAT API KEY --").split(config.api_keys.openweathermap).join("-- OPENWEATHERMAP API KEY --").split(config.api_keys.imgur).join("-- IMGUR API KEY --");
+	return data
+		.split(config.token).join("-- BOT TOKEN --")
+		.split(config.api_keys.bot_list["discordbots.org"]).join("-- discordbots.org API KEY --")
+		.split(config.api_keys.bot_list["bots.discord.pw"]).join("-- bots.discord.pw API KEY")
+		.split(config.api_keys.datadog.api_key).join("-- DATADOG API KEY --")
+		.split(config.api_keys.datadog.app_key).join("-- DATADOG APP KEY --")
+		.split(config.api_keys.rethink.host).join("-- RETHINKDB HOST IP ADDRESS --")
+		.split(config.api_keys.datadog.app_key).join("-- DATADOG APP KEY --")
+		.split(config.api_keys.youtube).join("-- YOUTUBE API KEY --")
+		.split(config.api_keys.steam).join("-- STEAM API KEY --")
+		.split(config.api_keys.thecatapi).join("-- RANDOM CAT API KEY --")
+		.split(config.api_keys.openweathermap).join("-- OPENWEATHERMAP API KEY --")
+		.split(config.api_keys.imgur).join("-- IMGUR API KEY --")
+		.split(config.api_keys.cleverbot[0]).join("-- CLEVERBOT USER KEY --")
+		.split(config.api_keys.cleverbot[1]).join("-- CLEVERBOT API KEY --");
 };
